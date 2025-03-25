e0a58f63fa814bb0489b6773cd83a0cfcfc4e589
thakuratul2 <pratapsinghatul111@gmail.com>
2025-03-25 21:55:04 +0530

    Added: Migration & Request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+    
+        $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
